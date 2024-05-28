a23bcdbf65f8fdc4ab926d3ddd414c07f5ce8626
student Pranali <pranalipandhare2002@gmail.com>
2024-05-28 16:54:52 +0530

    commit

--- src/Copmonents/LoginForm/LoginForm.jsx
+++ src/Copmonents/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LoginForm.css";
-import { FaUserAlt, FaLock } from "react-icons/fa";
+import { FaUserAlt, FaLock } from "react-icons/fa/index.js";
 
 const LoginForm = () => {
   return (
